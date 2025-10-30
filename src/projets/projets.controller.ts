aac7b4c9efc9019b7c31de4f9b9b0b097f31b44f
AngeloANN <angelondiforan@hotmail.com>
2025-10-30 11:05:22 -0400

    Ajout de l'action findAll (GET) au ProjetsController

--- src/projets/projets.controller.ts
+++ src/projets/projets.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('projets')
-export class ProjetsController {}
+export class ProjetsController {
+  @Get()
+  obtenirTous() {
+    const timestamp = new Date().toJSON();
+    const projets = [
+      {
+        id: 1,
+        nom: 'LAN Party (NestJS API)',
+        description: 'Organiser le LAN Party',
+        image_url: 'https://i.imgur.com/Y5nZ4Qe.jpg',
+        date_creation: timestamp,
+        date_modification: timestamp,
+      },
+      {
+        id: 2,
+        nom: 'Discreddit (NestJS API)',
+        description: 'Projet de session',
+        image_url: 'https://i.imgur.com/Y5nZ4Qe.jpg',
+        date_creation: timestamp,
+        date_modification: timestamp,
+      },
+    ];
+    return projets;
+  }
+}
