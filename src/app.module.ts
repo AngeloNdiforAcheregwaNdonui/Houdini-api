3739a691e9c48bb35661950122a384d8f101b62d
AngeloANN <angelondiforan@hotmail.com>
2025-11-06 15:04:44 -0500

    Changes a langue en défaut

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,30 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjetsModule } from './projets/projets.module';
 import { IssuesModule } from './issues/issues.module';
+import {
+  AcceptLanguageResolver,
+  HeaderResolver,
+  I18nModule,
+  QueryResolver,
+} from 'nestjs-i18n';
+import path from 'node:path';
 
 @Module({
-  imports: [ProjetsModule, IssuesModule],
+  imports: [
+      I18nModule.forRoot({
+        fallbackLanguage: 'fr',
+        loaderOptions: {
+          path: path.join(__dirname, '/i18n/'),
+          watch: true,
+        },
+        resolvers: [
+          { use: QueryResolver, options: ['lang'] },
+          AcceptLanguageResolver,
+          new HeaderResolver(['x-lang']),
+        ],
+      }),
+    ProjetsModule,
+    IssuesModule],
   controllers: [AppController],
   providers: [AppService],
 })
